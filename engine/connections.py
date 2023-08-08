416f588e313cf82e13a9863f38276e1b3faaf722
BarbosaPL <barbosa.pedro.it@gmail.com>
2023-08-07 22:33:07 -0300

    lição murano

--- engine/connections.py
+++ engine/connections.py
@@ -10,9 +10,9 @@ import zipfile
 year = datetime.date.today() - datetime.timedelta(days=60)
 year = year.strftime("%Y%m")
 
-pattern_file = f'FORNEC_{year}.ZIP'
+pattern_file = f"FORNEC_{year}.ZIP"
 file_to_download = f'ftp://ftp.datasus.gov.br/cnes/{pattern_file}'
-dir = "D:\Projetos\public-health\\"
+dir = "D:\Projetos\public-health\incoming_files\\"
 list_files = os.listdir(dir)
 
 print(list_files)
@@ -22,10 +22,12 @@ try:
     if file_to_download in list_files:
         print('Already Downloaded')
     else:
-        wget.download(file_to_download)
+        wget.download(file_to_download, out=dir)
         with zipfile.ZipFile(f'{dir}{pattern_file}', 'r') as zip_ref:
-            zip_ref.extractall(f'{dir}')
+            zip_ref.extractall(f'{dir}') #alterar essa função
 except EOFError as e:
-    print(e)
+    print(e) 
 
 
+# Extrair com o nome do arquivo _MESANO 
+#Exemplo: Fornec_202306.txt
