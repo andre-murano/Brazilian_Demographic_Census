f055580fe90edab1037d57eb2285ac381fecc50d
BarbosaPL <barbosa.pedro.it@gmail.com>
2023-08-08 20:42:38 -0300

    new file to ingest

--- engine/connections.py
+++ engine/connections.py
@@ -7,27 +7,23 @@ import datetime
 import zipfile
 
 # variables
-year = datetime.date.today() - datetime.timedelta(days=60)
+year = datetime.date.today() - datetime.timedelta(days=30)
 year = year.strftime("%Y%m")
 
 pattern_file = f"FORNEC_{year}.ZIP"
 file_to_download = f'ftp://ftp.datasus.gov.br/cnes/{pattern_file}'
-dir = "D:\Projetos\public-health\incoming_files\\"
+dir = f"D:\Projetos\public-health\incoming_files\\"
 list_files = os.listdir(dir)
 
-print(list_files)
+print(list_files) 
 
 # get file from FTP
 try:
-    if file_to_download in list_files:
+    if pattern_file in list_files:
         print('Already Downloaded')
     else:
         wget.download(file_to_download, out=dir)
         with zipfile.ZipFile(f'{dir}{pattern_file}', 'r') as zip_ref:
-            zip_ref.extractall(f'{dir}') #alterar essa função
+            zip_ref.extractall(f'{dir}')
 except EOFError as e:
     print(e) 
-
-
-# Extrair com o nome do arquivo _MESANO 
-#Exemplo: Fornec_202306.txt
--- engine/source/csv.py
+++ engine/source/csv.py
@@ -1,11 +1,4 @@
-# import libs
-import csv
-import io
-import re
-import wget
+import pandas as pd
 
-# connecting to source
-
-
-
-
+df1 = pd.read_csv(filepath_or_buffer='D:\Projetos\public-health\incoming_files\Fornec.txt',header=1,delimiter='\\t')
+print(df1.head(10))
