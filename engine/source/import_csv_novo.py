95f5f7ee30ffbe8c1a1ad46e42a2d7edb33de5fb
Andre Murano <andre.murano@hotmail.com>
2023-09-22 21:33:48 -0300

    Changes in import_csv

--- engine/source/import_csv_novo.py
+++ engine/source/import_csv_novo.py
@@ -1,13 +1,10 @@
+
 import pandas as pd
 from sqlalchemy import create_engine
 import os
 
-# from sqlalchemy import String, Integer
-
-
-
 # Diretório dos arquivos CSV
-dir = 'D:\\Projetos\\Projeto\\'
+dir = 'D:\\Projetos\\Projetos\\Brazilian_Census'
 list_files = os.listdir(dir)
 print(list_files)
 
@@ -29,7 +26,17 @@ for file in list_files:
     if '.csv' in file:
         nome_tabela = file.split(".")[0]
         nome_tabela_min = nome_tabela.lower()
-        tabela = pd.read_csv(file, delimiter=",", header=None, skiprows=1, names=colunas.values(), dtype={'munic_id': str, 'ano': int, 'sexo_id': int, 'situacao_id': str, 'fxetaria_id':int, 'populacao': int})
+        tabela = pd.read_csv(file, delimiter=",", header=None, skiprows=1, names=colunas.values(), encoding='latin1')
+        
+        # Preencher valores NA com -1 para colunas numéricas
+        tabela.fillna(-1, inplace=True)
+        
+        # Converter colunas para os tipos corretos
+        tabela['ano'] = tabela['ano'].astype(int)
+        tabela['sexo_id'] = tabela['sexo_id'].astype(int)
+        tabela['fxetaria_id'] = tabela['fxetaria_id'].astype(int)
+        tabela['populacao'] = tabela['populacao'].astype(int)
+        
         dict_df[nome_tabela_min] = tabela
 
 # Conexão com o banco de dados MySQL
@@ -39,5 +46,7 @@ db_connection = create_engine(db_connection_str)
 # Importar DataFrames nas tabelas MySQL
 for table_name, table_df in dict_df.items():
     table_df.to_sql(table_name, con=db_connection, if_exists='replace', index=False)
+
+
 
 
