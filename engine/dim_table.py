5d74f78c90efbebb84df2f4ce9cc147c6a540dd4
Andre Murano <andre.murano@hotmail.com>
2023-09-22 22:13:34 -0300

    dim_table modified

--- engine/dim_table.py
+++ engine/dim_table.py
@@ -1,29 +1,118 @@
-
-#Download the file with guidance on fact table columns
-
+import pandas as pd
+from sqlalchemy import create_engine
+import os
 import wget
 import os
 import zipfile
 from ftplib import FTP
+import mysql.connector
 
 
+#Download the file with guidance on fact table columns
 
 
-ftp = FTP("ftp.datasus.gov.br")
-ftp.login() 
-
-ftp.cwd("/dissemin/publicos/IBGE/doc/")  
-
-guidance_table = 'populacao.pdf'
+dir = 'D:\\Projetos\\Projetos\\Brazilian_Census'
+list_files = os.listdir(dir)
+filename = 'populacao.pdf'
 
 
-with open(guidance_table, 'wb') as arquivo:
-    ftp.retrbinary('RETR ' + guidance_table, arquivo.write)
+try:
+
+    for files in list_files:
+        if files == filename:
+            print('Already Downloaded')
+
+        else:
+            ftp = FTP("ftp.datasus.gov.br")
+            ftp.login() 
+
+            ftp.cwd("/dissemin/publicos/IBGE/doc/")  
+
+            guidance_table = 'populacao.pdf'
+
+
+            with open(guidance_table, 'wb') as arquivo:
+                ftp.retrbinary('RETR ' + guidance_table, arquivo.write)
+
+except Exception as e:
+    print(e)
+
+
+#DB Connection
+
+db_connection = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='112233',
+    database='projeto1'
+)
+
+# Cursor SQL
+cursor = db_connection.cursor()
 
 
 
 
 #Building the dim_tables
+
+tabela_sexo = {
+    1: 'Masculino',
+    2: 'Feminino'
+}
+
+criar_tabela_sexo = """
+CREATE TABLE IF NOT EXISTS sexo_tabela (
+    sexo_id INT PRIMARY KEY,
+    sexo VARCHAR(20)
+)
+"""
+
+cursor.execute(criar_tabela_sexo)
+
+
+
+criar_tabela_situacao = """
+CREATE TABLE IF NOT EXISTS situacao (
+    situacao_id INT PRIMARY KEY,
+    situacao VARCHAR(20)
+)
+"""
+
+cursor.execute(criar_tabela_situacao)
+
+
+
+
+
+
+# Dados para inserção
+
+dados_sexo = [
+    (1, 'Masculino'),
+    (2, 'Feminino')
+]
+
+dados_situacao = [
+    (1,'Urbano'),
+    (2, 'Rural'),
+    (3, 'Não Levantado')
+    ] 
+
+
+# Inserção dos valores
+
+inserir_dados_sexo = "INSERT INTO sexo_tabela (sexo_id, sexo) VALUES (%s, %s)"
+cursor.executemany(inserir_dados_sexo, dados_sexo)
+
+inserir_dados_situacao = "INSERT INTO situacao (situacao_id, situacao) VALUES (%s, %s)"
+cursor.executemany(inserir_dados_situacao, dados_situacao)
+
+# Commit das alterações
+db_connection.commit()
+
+# Fechar conexão
+cursor.close()
+db_connection.close()
 
 
 
--- engine/source/import_csv_novo.py
+++ engine/source/import_csv_novo.py
@@ -3,12 +3,12 @@ import pandas as pd
 from sqlalchemy import create_engine
 import os
 
-# Diretório dos arquivos CSV
+
 dir = 'D:\\Projetos\\Projetos\\Brazilian_Census'
 list_files = os.listdir(dir)
 print(list_files)
 
-# Mapeamento dos nomes das colunas
+
 colunas = {
     'coluna1': 'munic_id',
     'coluna2': 'ano',
@@ -18,20 +18,21 @@ colunas = {
     'coluna6': 'populacao'
 }
 
-# Dicionário para armazenar os DataFrames
+
 dict_df = {}
 
-# Ler arquivos CSV e renomear colunas
+
 for file in list_files:
     if '.csv' in file:
         nome_tabela = file.split(".")[0]
         nome_tabela_min = nome_tabela.lower()
         tabela = pd.read_csv(file, delimiter=",", header=None, skiprows=1, names=colunas.values(), encoding='latin1')
         
-        # Preencher valores NA com -1 para colunas numéricas
+    
         tabela.fillna(-1, inplace=True)
         
-        # Converter colunas para os tipos corretos
+
+   
         tabela['ano'] = tabela['ano'].astype(int)
         tabela['sexo_id'] = tabela['sexo_id'].astype(int)
         tabela['fxetaria_id'] = tabela['fxetaria_id'].astype(int)
@@ -39,11 +40,12 @@ for file in list_files:
         
         dict_df[nome_tabela_min] = tabela
 
-# Conexão com o banco de dados MySQL
+
+# MySQL Connection
 db_connection_str = f"mysql+mysqlconnector://root:112233@localhost/projeto1"
 db_connection = create_engine(db_connection_str)
 
-# Importar DataFrames nas tabelas MySQL
+#Import Dataframes
 for table_name, table_df in dict_df.items():
     table_df.to_sql(table_name, con=db_connection, if_exists='replace', index=False)
 
